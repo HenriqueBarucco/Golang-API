a20720b3f539ac738ef8649dbc48a5d66c561769
Henrique Barucco <henrique_barucco@hotmail.com>
2023-01-22 19:39:41 -0300

    Conectando com o Banco

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/henriquebarucco/Golang-API/database"
 	"github.com/henriquebarucco/Golang-API/models"
 	"github.com/henriquebarucco/Golang-API/routes"
 )
@@ -12,6 +13,8 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
+
+	database.ConectaComBancoDeDados()
 
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
